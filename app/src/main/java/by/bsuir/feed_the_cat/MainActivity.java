12ca0335ea109d09db6820a8dca6ebf9f97ffd90
suprugafad <poidaalesya382@gmail.com>
2022-04-12 19:56:23 +0300

    add sharing in social networks

--- app/src/main/java/by/bsuir/feed_the_cat/MainActivity.java
+++ app/src/main/java/by/bsuir/feed_the_cat/MainActivity.java
@@ -2,11 +2,14 @@ package by.bsuir.feed_the_cat;
 
 import static java.lang.Integer.parseInt;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -72,6 +75,42 @@ public class MainActivity extends AppCompatActivity {
         feedButton.setOnClickListener(oclFeedButton);
         homeButton.setOnClickListener(oclHomeButton);
 
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        // first parameter is the file for icon and second one is menu
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        // We are using switch case because multiple icons can be kept
+        switch (item.getItemId()) {
+            case R.id.shareButton:
+
+                Intent sharingIntent = new Intent(Intent.ACTION_SEND);
+
+                // type of the content to be shared
+                sharingIntent.setType("text/plain");
+
+                // Body of the content
+                String shareBody = "My result: " + satiety;
+
+                // subject of the content. you can share anything
+                String shareSubject = "Result in feed the cat";
+
+                // passing body of the content
+                sharingIntent.putExtra(Intent.EXTRA_TEXT, shareBody);
+
+                // passing subject of the content
+                sharingIntent.putExtra(Intent.EXTRA_SUBJECT, shareSubject);
+                startActivity(Intent.createChooser(sharingIntent, "Share using"));
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
