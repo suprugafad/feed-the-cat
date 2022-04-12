398022a9f1e03482a1b721d43239ae674b187fa0
suprugafad <poidaalesya382@gmail.com>
2022-04-12 15:47:57 +0300

    add cats animation

--- app/src/main/java/by/bsuir/feed_the_cat/MainActivity.java
+++ app/src/main/java/by/bsuir/feed_the_cat/MainActivity.java
@@ -5,22 +5,28 @@ import static java.lang.Integer.parseInt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
-import android.content.SharedPreferences;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
     Button feedButton;
+    Button homeButton;
     TextView textValueOfSatiety;
-    ImageView cat;
+    ImageView catImg;
 
     int satiety;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,9 +36,21 @@ public class MainActivity extends AppCompatActivity {
         setTitle(R.string.title);
 
         feedButton = (Button) findViewById(R.id.feed_button);
+        homeButton = (Button) findViewById(R.id.home_button);
         textValueOfSatiety = (TextView) findViewById(R.id.value_of_satiety);
+        catImg = (ImageView) findViewById(R.id.cat_img);
 
         satiety = parseInt(textValueOfSatiety.getText().toString());
+
+        Intent stActivity = new Intent(this, StartActivity.class);
+
+        final Animation animationRotateCenter = AnimationUtils.loadAnimation(
+                this, R.anim.cat_rotation);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello");
 
         View.OnClickListener oclFeedButton = new View.OnClickListener() {
             @SuppressLint("SetTextI18n")
@@ -41,14 +59,20 @@ public class MainActivity extends AppCompatActivity {
                 satiety += 1;
                 textValueOfSatiety.setText(Integer.toString(satiety));
 
-//                if (satiety % 15 == 0) {
-//
-//                }
+                if (satiety % 15 == 0)
+                    catImg.startAnimation(animationRotateCenter);
 
             }
         };
 
+        View.OnClickListener oclHomeButton = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(stActivity);
+            }
+        };
         feedButton.setOnClickListener(oclFeedButton);
+        homeButton.setOnClickListener(oclHomeButton);
 
     }
 
--- app/src/main/java/by/bsuir/feed_the_cat/StartActivity.java
+++ app/src/main/java/by/bsuir/feed_the_cat/StartActivity.java
@@ -1,9 +1,7 @@
 package by.bsuir.feed_the_cat;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
